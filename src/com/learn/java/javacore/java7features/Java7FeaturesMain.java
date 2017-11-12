fafa9605a22dd3cce2f7f837e02d39ed47727c54
Ionut Barau <ionutbarau@efficientix.go.ro>
2017-11-12 21:27:41 +0100

    Added new methods for java 7 features

--- src/com/learn/java/javacore/java7features/Java7FeaturesMain.java
+++ src/com/learn/java/javacore/java7features/Java7FeaturesMain.java
@@ -1,6 +1,9 @@
 package com.learn.java.javacore.java7features;
 
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 
 /**
@@ -11,12 +14,15 @@ import java.sql.SQLException;
  */
 public class Java7FeaturesMain {
 
-    public static final int NUMERIC_LITERAL_WITH_UNDERSCORE = 10_000;
+    public static final Integer NUMERIC_LITERAL_WITH_UNDERSCORE = 10_000;
 
     public static void main(String[] args){
         multiCatchAndStringsInSwitchExample("a");
         multiCatchAndStringsInSwitchExample("b");
         multiCatchAndStringsInSwitchExample("c");
+
+        tryWithResources();
+        nio();
     }
 
 
@@ -33,6 +39,35 @@ public class Java7FeaturesMain {
             }
         }catch(SQLException | FileNotFoundException e){
             System.out.println(e.getClass() + " caught !");
+        }
+    }
+
+    public static void tryWithResources(){
+        try(
+                FileReader fr = new FileReader("src/com/learn/java/javacore/java7features/tryWithResources.txt");
+                BufferedReader br = new BufferedReader(fr)
+        ){
+            String msg;
+            while((msg = br.readLine()) != null) {
+                System.out.println(msg);
+            }
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void nio(){
+        String directoryPath = "src/com/learn/java/javacore/java7features/nio";
+        try {
+            Path nioDirectory = Files.createDirectories(Paths.get(directoryPath));
+            Path nioFile = Files.createFile(Paths.get(directoryPath,"nio.txt"));
+            Files.write(nioFile, NUMERIC_LITERAL_WITH_UNDERSCORE.toString().getBytes());
+            byte[] msg = Files.readAllBytes(nioFile);
+            System.out.println(new String(msg));
+            Files.deleteIfExists(nioFile);
+            Files.deleteIfExists(nioDirectory);
+        }catch (IOException e){
+            System.out.println("Exception : " + e.getMessage());
         }
     }
 }
