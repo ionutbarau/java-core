6b7f74eca65b6b4850b29644b1e61b1d32a95945
ionutbarau <ionutbarau@gmail.com>
2018-03-21 13:27:43 +0100

    Added ForkJoin and WatcherService from NIO example

--- src/com/learn/java/javacore/java7features/Java7FeaturesMain.java
+++ src/com/learn/java/javacore/java7features/Java7FeaturesMain.java
@@ -1,10 +1,17 @@
 package com.learn.java.javacore.java7features;
 
 import java.io.*;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
 import java.sql.SQLException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
 
 /**
  * User: Ionut Barau (ionutbarau)
@@ -16,13 +23,15 @@ public class Java7FeaturesMain {
 
     public static final Integer NUMERIC_LITERAL_WITH_UNDERSCORE = 10_000;
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException, InterruptedException {
         multiCatchAndStringsInSwitchExample("a");
         multiCatchAndStringsInSwitchExample("b");
         multiCatchAndStringsInSwitchExample("c");
 
         tryWithResources();
         nio();
+        forkJoin();
+        System.exit(1);
     }
 
 
@@ -56,18 +65,44 @@ public class Java7FeaturesMain {
         }
     }
 
-    public static void nio(){
+    public static void nio() throws IOException, InterruptedException {
+        WatchService watchService = FileSystems.getDefault().newWatchService();
         String directoryPath = "src/com/learn/java/javacore/java7features/nio";
-        try {
-            Path nioDirectory = Files.createDirectories(Paths.get(directoryPath));
-            Path nioFile = Files.createFile(Paths.get(directoryPath,"nio.txt"));
-            Files.write(nioFile, NUMERIC_LITERAL_WITH_UNDERSCORE.toString().getBytes());
-            byte[] msg = Files.readAllBytes(nioFile);
-            System.out.println(new String(msg));
-            Files.deleteIfExists(nioFile);
-            Files.deleteIfExists(nioDirectory);
-        }catch (IOException e){
-            System.out.println("Exception : " + e.getMessage());
+
+        Path nioDirectory = Files.createDirectories(Paths.get(directoryPath));
+        nioDirectory.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY);
+        Path nioFile = Files.createFile(Paths.get(directoryPath,"nio.txt"));
+        Files.write(nioFile, NUMERIC_LITERAL_WITH_UNDERSCORE.toString().getBytes());
+        byte[] msg = Files.readAllBytes(nioFile);
+        System.out.println(new String(msg));
+        Files.deleteIfExists(nioFile);
+        Files.deleteIfExists(nioDirectory);
+        while(true){
+            WatchKey key = watchService.take();
+            for(WatchEvent<?> event : key.pollEvents()) {
+
+                WatchEvent.Kind<?> kind = event.kind();
+
+                System.out.println("Event on "+ event.context().toString() + " is "+ kind);
+
+            }
+            break;
+        }
+    }
+
+    public static void forkJoin(){
+        int numberOfCPU = Runtime.getRuntime().availableProcessors();
+        System.out.println("Running fork and join on " + numberOfCPU + " processors...");
+        ForkJoinPool pool = new ForkJoinPool(numberOfCPU);
+        pool.invoke(new MyTask());
+
+    }
+
+    public static class MyTask extends RecursiveAction{
+
+        @Override
+        protected void compute() {
+            System.out.println("Compute..");
         }
     }
 }
